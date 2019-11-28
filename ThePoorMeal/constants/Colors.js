d3ba3125f90e1b70d93af5dd8407e687b5bdd853
Abhik <finnabc123@gmail.com>
2019-11-28 07:08:33 +0530

    Adding Tab bar effect

--- ThePoorMeal/constants/Colors.js
+++ ThePoorMeal/constants/Colors.js
@@ -1,4 +1,5 @@
 export default {
     primaryColor: '#e2eddf',
-    accentColor: '#bf7f7e'
+    accentColor: '#bf7f7e',
+    headerTintColor: '#6c756c'
 }
--- ThePoorMeal/navigations/MealsNavigator.js
+++ ThePoorMeal/navigations/MealsNavigator.js
@@ -1,6 +1,10 @@
+import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createAppContainer } from 'react-navigation';
+import { Ionicons } from '@expo/vector-icons';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
+import { Platform } from 'react-native';
 
 
 import CategoriesScreen from '../screens/CategoriesScreen';
@@ -8,6 +12,7 @@ import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailsScreen from '../screens/MealDetailsScreen';
 import Colors from '../constants/Colors';
 import FavoritesScreen from '../screens/FavoritesScreen';
+
 
 const MealsNavigator = createStackNavigator({
     Categories: {
@@ -23,15 +28,40 @@ const MealsNavigator = createStackNavigator({
         headerStyle: {
             backgroundColor: Colors.primaryColor
         },
-        headerTintColor: '#6c756c',
+        headerTintColor: Colors.headerTintColor,
         headerTitle: 'Help'
     },
 
 })
 
-const MealsFavTabNavigator = createBottomTabNavigator({
-    Meals: MealsNavigator,
-    Favorites: FavoritesScreen
-});
+const tabScreenConfig = {
+    Meals: {
+        screen: MealsNavigator, navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
+            },
+            tabBarColor: Colors.primaryColor,
+        }
+    },
+    Favorites: {
+        screen: FavoritesScreen, navigationOptions: {
+            tabBarLabel: 'Favorites!',
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />
+            },
+            tabBarColor: Colors.headerTintColor
+        }
+    }
+}
+
+const MealsFavTabNavigator = Platform.OS === 'android' ?
+    createMaterialBottomTabNavigator(tabScreenConfig, {
+        shifting: true
+    }) :
+    createBottomTabNavigator(tabScreenConfig, {
+        tabBarOptions: {
+            activeTintColor: Colors.headerTintColor
+        }
+    });
 
 export default createAppContainer(MealsFavTabNavigator);
